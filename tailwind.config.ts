0e270bbdfd14363577d4834a8607dbc11da9cc84
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-12-14 21:42:40 +0530

    Color added to the tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        'my-color-1': '#425059',
+        'my-color-2': '#BDBFB8',
+        'my-color-3': '#F2F1DF',
+        'my-color-4': '#F2C744',
+        'my-color-5': '#F2B035',
+        'my-color-6': '#F2E641',
+        'my-color-7': '#687E8C',
+        'my-color-8': '#735A10'
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
