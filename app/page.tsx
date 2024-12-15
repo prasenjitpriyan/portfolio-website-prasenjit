2207ca0245ac1a9f13eb2cd58a8f59caa0386c03
PRASENJIT DAS <prasenjitdas@PRASENJITs-MacBook-Air.local>
2024-12-15 08:45:08 +0530

    Some components removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import HeroSection from '@/components/hero-section'
 import KeyStats from '@/components/key-stats'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className="flex flex-col">
-      <HeroSection />
       <KeyStats />
     </div>
   )
